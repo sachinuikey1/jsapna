32cc095cc5f456ac6cb6dce4c557f2a38c413b27
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-24 12:03:03 +0000

    Efelse statement

---
+++ Ifelse/jsifelse.js
@@ -0,0 +1,31 @@
+//conditional statements
+
+// console.log("before my if statement");
+// let age = 17;
+//     if(age >= 18){
+//     console.log("you can vote");
+//     console.log("you can drive");
+// }
+// if(age < 18){
+//     console.log("you cannot vote");
+
+// }
+// console.log("after my if statement");
+
+
+// let firstName = "mangesh";
+// if (firstName == "sachin"){
+//     console.log(`Welcome ${firstName}`);
+// }
+
+let color = "red";
+
+if (color == "red"){
+    console.log("Stop the car");
+}
+if (color == "yellow"){
+    console.log("stow down the car");
+}
+if (color == "green"){
+    console.log("go")
+}
