f93370404251b9a4060dd797ae5563e8d7e36ce1
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-25 20:55:13 +0000

    Array ds

--- JSPART3/ArrayDS.js
+++ JSPART3/ArrayDS.js
@@ -22,10 +22,68 @@
 // let name = "sachin";
 // console.log(name[3]);
 
-let cars = ["audi", "maruti","bmw","xuv"];
-console.log(cars.push("toyota"));
-console.log(cars.push("farrari"));
-console.log(cars.pop());
+// let cars = ["audi", "maruti","bmw","xuv"];
+// console.log(cars.push("toyota"));
+// console.log(cars.push("farrari"));
+// console.log(cars.pop());
+
+// console.log(cars.unshift("mahindra"));
+// console.log(cars);
+
+// console.log(cars.shift());
+
+// let followers = ["a", "b", "c"];
+// let blocked = followers.shift();
+
+// console.log(blocked);
+// console.log(followers);
+
+// let month = ["january", "july", "march", "august"];
+
+// console.log(month.shift());
+// console.log(month);
+
+// console.log(month.shift());
+// console.log(month);
+
+// console.log(month.unshift("june"));
+// console.log(month);
+
+// console.log(month.unshift("july"));
+// console.log(month);
 
 
+// Array Method.
+
+let primary = ["red", "green", "blue"];
+let secondary = ["grey", "orange"];
+
+// console.log(primary.indexOf("green"));
+// console.log(primary);
+
+// includes means boolean value true or false exist value or not.
+
+// console.log(primary.includes("red"));
+// console.log(primary.includes("pink"));
+
+
+// Concat Method.
+
+// console.log(primary.concat(secondary));
+
+// Reverse string.
+
+// console.log(primary.reverse());
+
+// Slice of Arrays.
+
+// console.log(primary.slice());
+// console.log(primary);
+
+// console.log(primary.slice(1));
+
+let months = ["january", "july", "march", "august"];
+
+console.log(months.splice(0,2, "july", "june") );
+console.log(months);
 
