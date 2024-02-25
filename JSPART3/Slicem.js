23e3e060c571db1e2a3db81fa6ac4f3eca1cb855
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-25 20:02:28 +0000

    Slice method

---
+++ JSPART3/Slicem.js
@@ -0,0 +1,15 @@
+// slice
+// Returns a part of the original as a new string.
+
+// let str = "loveCoding";
+// console.log(str.slice(4, 10));
+
+// // REPLACE
+
+// let msg = "collage";
+// console.log(msg.replace('a', 'x'));
+// console.log(msg.repeat(3));
+
+// let msg = "    help    ";
+// console.log(msg.trim().toUpperCase());
+
