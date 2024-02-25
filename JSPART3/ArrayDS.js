a4a6d2fd7b1ade16d569b064f884338aa6a6591c
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-25 20:01:21 +0000

    Arrays

---
+++ JSPART3/ArrayDS.js
@@ -0,0 +1,31 @@
+// Array(Data structure)
+
+// Linear collection of things.
+
+
+// let student1 = "sachin";
+// let student2 = "paras";
+// let student3 = "dipesh";
+
+// console.log
+
+// let students = ["sachin","paras","dipesh"];
+// console.log(students);
+
+
+// Visualization Array.
+
+// let nums = [2, 4, 6, 8];
+
+// console.log(nums[1]);
+
+// let name = "sachin";
+// console.log(name[3]);
+
+let cars = ["audi", "maruti","bmw","xuv"];
+console.log(cars.push("toyota"));
+console.log(cars.push("farrari"));
+console.log(cars.pop());
+
+
+
