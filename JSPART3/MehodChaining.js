1161eaa77e6bd1b1e10afb86e624d65b229c8866
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-25 20:02:07 +0000

    methods

---
+++ JSPART3/MehodChaining.js
@@ -0,0 +1,13 @@
+// Methdod Chaining
+
+// Using one method after another. order of execution wiil be left to right.
+
+let msg = "  hellow   ";
+// let newMsg = msg.trim();
+// console.log(newMsg);
+
+// newMsg = newMsg.toUpperCase();
+// console.log(newMsg);
+
+let newMsg = msg.trim().toUpperCase();
+console.log(newMsg);
