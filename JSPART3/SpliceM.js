5bc4df7c06d722f93e03559f593153662a9bc082
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-25 20:55:34 +0000

    Splice method

---
+++ JSPART3/SpliceM.js
@@ -0,0 +1,35 @@
+// splice : removes/ replaces / add elements in place.
+// splice (start,deleteCount, item0...itemN);
+
+// let cars = ["audi", "bmw", "xuv", "maruti"];
+
+// console.log(cars.splice(3));
+// console.log(cars);
+
+// console.log(cars.splice(0,1));
+// console.log(cars);
+
+// console.log(1,1, "marcedes");
+// console.log(cars);
+
+
+// console.log(1,1, "creta");
+// console.log(cars);
+
+// output
+
+// @sachinuikey1 ➜ /workspaces/jsapna (main) $ node JSPART3/SpliceM.js
+// [ 'maruti' ]
+// [ 'audi', 'bmw', 'xuv' ]
+// [ 'audi' ]
+// [ 'bmw', 'xuv' ]
+
+// SORT METHOD.
+
+// let days = ["monday", "tuesday", "wednesday", "thrusday", "friday", "saturday", "sunday"];
+
+// console.log(days.sort());
+
+// let marks = [99, 100, 55, 90, 33];
+
+
