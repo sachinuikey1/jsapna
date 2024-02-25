c77f49545dc0a9b68c2ef392eeee4ddbfc0911a5
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-25 20:02:48 +0000

    String method

---
+++ JSPART3/stringm.js
@@ -0,0 +1,38 @@
+// Method- actions that can be performed on objects.
+
+// FORMAT
+
+// strng are Immutable in JS.
+
+// No change can be made to strings.
+// whenever we do try to make a change, a new string is created and old one remains same.
+
+
+
+
+// str.trime() // trime() to space are cover to js.
+
+// let msg = "  hellow  ";  space are extra in string remove.
+// console.log(msg);
+
+// let password = prompt("set your password");
+// let newpass = password.trim();
+
+// console.log(newpass);
+
+// str.toUpperCase()
+// str.toLowerCase()
+
+let name = "apna college";
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+
+// String Methods with Arguments
+// argumet is a some value that we pass to the method.
+
+// Format
+// StringName.method(arg)
+
+let str = "IloveCoding";
+
+console.log(str.indexOf("love"));
