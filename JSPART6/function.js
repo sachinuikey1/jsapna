3d6c605372a488e882096948783ec413703b133f
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-28 20:56:22 +0000

    FUNCTIONS

---
+++ JSPART6/function.js
@@ -0,0 +1,71 @@
+// function functionName(){
+     //do something
+// }
+
+// function hello(){
+//     console.log("hello");
+// }
+// hello();
+// hello();
+// hello();
+// hello();
+
+// function printName(){
+//     console.log("apna collage");
+// }
+// printName();
+
+// function print1to5(){
+//     for(let i=1; i<=5; i++){
+//         console.log(i);
+//     }
+
+// }
+// print1to5();
+
+// function isAdult(){
+//     let age = 18;
+//     if(age >= 18){
+//         console.log("adult");
+//     }else{
+//         console.log("not adult");
+//     }
+// }
+// isAdult();
+
+
+
+// function rollDice(){
+//     let rand = Math.floor(Math.random() * 6) +1;
+//     console.log(rand);
+// }
+
+// rollDice();
+// rollDice();
+// rollDice();
+// rollDice();
+// rollDice();
+// rollDice();
+
+// function printInfo(name, age){
+//     console.log(`${name}'s age is ${age}.`);
+// }
+
+// console.log("sachin", 26);
+// console.log("paras",25);
+// console.log("dipesh");
+
+// function calcAvg(a,b,c){
+//     console.log(a+b+c)/3;
+// }
+// calcAvg(3, 6, 8)/3;
+
+function printTable(n){
+    for(let i=n; i<=n*10; i+=n){
+        console.log(i);
+    }
+}
+
+printTable(2);
+printTable(3);
+printTable(56);
