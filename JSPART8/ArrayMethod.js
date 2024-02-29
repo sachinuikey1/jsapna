f5784dc53ff6b6683ea55bf7d431704b372f50e7
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-29 19:44:23 +0000

    Array Method

---
+++ JSPART8/ArrayMethod.js
@@ -0,0 +1,29 @@
+// let arr = [1,2,3,4,5];
+
+// arr.forEach( (el) => {
+//     console.log(el);
+// })
+
+// arr.forEach(function (el) {
+//     console.log(el);
+// });
+
+// let print = function (el){
+//     console.log(el);
+// };
+
+// arr.forEach(print);
+
+let arr = [{
+    name: "sachin",
+    marks: 90
+},
+{
+    name: "samir",
+    marks: 87
+},
+{
+    name: "komal",
+    marks: 99
+},
+]
