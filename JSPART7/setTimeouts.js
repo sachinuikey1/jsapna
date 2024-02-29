bb78fe7f30f7b559546f8c83c2bf3566deea23f6
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-29 12:54:58 +0000

    SetTimeout

---
+++ JSPART7/setTimeouts.js
@@ -0,0 +1,24 @@
+
+
+// SetTimeout
+
+// setTimeout(function, timeout)
+
+// console.log("hi there");
+
+// setTimeout( () => {
+//     console.log("Apna desh");
+// }, 4000);
+
+// console.log("Welcome!!...");
+
+
+// setInterval
+
+// setInterval(function, timeout)
+
+let id = setInterval( () => {
+    console.log("Apna desh");
+}, 2000);
+
+console.log(id);
