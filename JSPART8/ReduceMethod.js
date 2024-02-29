e49eea67520f3f4d3109979130c1ff06e8039f30
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-29 19:45:53 +0000

    Reduce Method

---
+++ JSPART8/ReduceMethod.js
@@ -0,0 +1,14 @@
+// // Reduce the array to a single value.
+
+// arr.reduce( reduce function with 2 variables for (accumulator, element));
+
+
+console.log([1,2,3,4].reduce( (res, el) => (res+el) ));
+
+
+// OUTPUT
+// @sachinuikey1 ➜ /workspaces/jsapna (main) $ node JSPART8/ReduceMethod.js
+// 10
+// @sachinuikey1 ➜ /workspaces/jsapna (main) $ 
+
+
