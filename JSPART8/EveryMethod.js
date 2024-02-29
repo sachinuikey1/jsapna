95d22264e1b79fda4a947cbe00735fe49898c1d3
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-02-29 19:45:18 +0000

    Every & some method

---
+++ JSPART8/EveryMethod.js
@@ -0,0 +1,9 @@
+// returns true if  every element of arryay gives true for some function. Else returns false.
+
+// arr.every(some function definition or name);
+
+console.log([2,4,6].every((el) => el % 2 == 0));
+
+console.log([2,4,6, 9].every((el) => el % 2 == 0));
+
+
