d46089f30848b02fa5c974dd19765d0cc0514e48
sachinuikey1 <121891192+sachinuikey1@users.noreply.github.com>
2024-03-05 07:24:52 +0000

    Async Keyword

---
+++ JSPART12/AsyncKeyword.js
@@ -0,0 +1,10 @@
+// Creates an Async function -> promise  catch & then menthod use..
+
+
+async function greet() {
+    return "hello world"  //return a promise.
+}
+
+let hello = async () => {}; //returns a promise.
+
+console.log(greet);
